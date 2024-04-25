8c6b78e6bc0afd5622249415aaa44189a38ad633
uosjapuelks <anderson@u.nus.edu>
2024-04-25 10:43:03 +0800

    Initial .dotfile commits

---
+++ .config/nvim/lua/uosjapue/core/options.lua
@@ -0,0 +1,33 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
