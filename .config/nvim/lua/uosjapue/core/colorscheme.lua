49ae6b6e45503a710dc41ecd093950bc10f0a4d4
uosjapuelks <anderson@u.nus.edu>
2025-02-18 20:51:56 +0800

    made nvim bg transparent

--- .config/nvim/lua/uosjapue/core/colorscheme.lua
+++ .config/nvim/lua/uosjapue/core/colorscheme.lua
@@ -1,6 +1,15 @@
-local status, _ = pcall(vim.cmd, "colorscheme kanagawa-wave")
+local status, _ = pcall(vim.cmd, "colorscheme kanagawa-dragon")
 if not status then
   print("colorscheme not found")
   return
 end
 
+-- require("kanagawa").load("wave")
+
+-- Make BG transparent
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
