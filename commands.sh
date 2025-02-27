450e3d267371364308ba5db6c3df3c47f2b3c647
anderson <aksleong@gmail.com>
2025-02-28 02:49:30 +0800

    Use zsh plugins

--- commands.sh
+++ commands.sh
@@ -1,5 +1,6 @@
 sudo apt install tree curl
 
+# FONT
 wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Meslo.zip \
 && cd ~/.local/share/fonts \
 && unzip Meslo.zip \
@@ -11,3 +12,10 @@ sudo apt install gcc clang
 
 # Ubuntu24 swap caps and ctrl
 # gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swapcaps']"
+
+# Install zsh
+sudo apt install zsh
+chsh zsh
+
+# Install Oh My Posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
