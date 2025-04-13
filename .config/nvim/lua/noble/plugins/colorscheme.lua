17af98463c39451447c409595f79182759eb3c27
Anderson Leong <aksleong@gmail.com>
2025-04-13 17:58:13 +0000

    feat: Transparent colorscheme
    Transparent Gruvbox Dark default colorscheme

--- .config/nvim/lua/noble/plugins/colorscheme.lua
+++ .config/nvim/lua/noble/plugins/colorscheme.lua
@@ -13,26 +13,29 @@ return {
 			-- local fg_gutter = "#627E97"
 			-- local border = "#547998"
 
-			-- require("gruvbox").setup({
-			-- style = "dark",
-			-- on_colors = function(colors)
-			--   colors.bg = bg
-			--   colors.bg_dark = bg_dark
-			--   colors.bg_float = bg_dark
-			--   colors.bg_highlight = bg_highlight
-			--   colors.bg_popup = bg_dark
-			--   colors.bg_search = bg_search
-			--   colors.bg_sidebar = bg_dark
-			--   colors.bg_statusline = bg_dark
-			--   colors.bg_visual = bg_visual
-			--   colors.border = border
-			--   colors.fg = fg
-			--   colors.fg_dark = fg_dark
-			--   colors.fg_float = fg
-			--   colors.fg_gutter = fg_gutter
-			--   colors.fg_sidebar = fg_dark
-			-- end,
-			-- })
+			require("gruvbox").setup({
+				terminal_colors = true,
+				style = "dark",
+				transparent_mode = true,
+
+				-- on_colors = function(colors)
+				-- 	colors.bg = bg
+				-- 	colors.bg_dark = bg_dark
+				-- 	colors.bg_float = bg_dark
+				-- 	colors.bg_highlight = bg_highlight
+				-- 	colors.bg_popup = bg_dark
+				-- 	colors.bg_search = bg_search
+				-- 	colors.bg_sidebar = bg_dark
+				-- 	colors.bg_statusline = bg_dark
+				-- 	colors.bg_visual = bg_visual
+				-- 	colors.border = border
+				-- 	colors.fg = fg
+				-- 	colors.fg_dark = fg_dark
+				-- 	colors.fg_float = fg
+				-- 	colors.fg_gutter = fg_gutter
+				-- 	colors.fg_sidebar = fg_dark
+				-- end,
+			})
 			-- load the colorscheme here
 			vim.cmd([[colorscheme gruvbox]])
 			-- vim.o.background = "light"
