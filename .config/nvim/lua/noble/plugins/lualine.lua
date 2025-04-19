d1d74d7f29b85a72f29a081d4a37b74a69a718aa
uosjapuelks <anderson@u.nus.edu>
2025-04-19 10:52:36 +0800

    feat: Codeium AI plugin
    Implement Codeium Plugins

--- .config/nvim/lua/noble/plugins/lualine.lua
+++ .config/nvim/lua/noble/plugins/lualine.lua
@@ -22,12 +22,31 @@ return {
 						cond = lazy_status.has_updates,
 						color = { fg = "#ff9e64" },
 					},
+					{
+						function()
+							local status = require("codeium.virtual_text").status()
+							if status.state == "idle" then
+								return "♿︎"
+							end
+							if status.state == "waiting" then
+								return "⏲ waiting... "
+							end
+							if status.state == "completions" and status.total > 0 then
+								return string.format("✆ %d/%d", status.current, status.total)
+							end
+							return " 0 "
+						end,
+					},
 					{ "encoding" },
 					{ "fileformat" },
 					{ "filetype" },
 				},
 			},
 		})
+
+		require("codeium.virtual_text").set_statusbar_refresh(function()
+			require("lualine").refresh()
+		end)
 	end,
 }
 
--- .config/nvim/lua/noble/plugins/nvim-cmp.lua
+++ .config/nvim/lua/noble/plugins/nvim-cmp.lua
@@ -14,6 +14,7 @@ return {
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
 		"onsails/lspkind.nvim", -- vs-code like pictograms
+		"Exafunction/windsurf.nvim", -- Codeium AI
 	},
 	config = function()
 		local cmp = require("cmp")
@@ -21,6 +22,43 @@ return {
 		local luasnip = require("luasnip")
 
 		local lspkind = require("lspkind")
+
+		local codeium_ok, codeium = pcall(require, "codeium")
+		codeium.setup({
+			enable_cmp_source = false,
+			virtual_text = {
+				enabled = true,
+				filetypes = {
+					markdown = false,
+					yaml = false,
+					json = false,
+					xml = false,
+				},
+				default_filetype_enabled = true,
+
+				-- Set to true if never want completions shown automatically
+				manual = true,
+				idle_delay = 75, -- (ms)
+
+				map_keys = true,
+				key_bindings = {
+					accept = "<C-Enter>",
+					accept_word = "<C-'>",
+					accept_line = "<C-;>",
+					next = "<C-Down>",
+					prev = "<C-Up>",
+				},
+			},
+		})
+
+		require("lazy").load({ plugins = { "windsurf.nvim" } })
+		vim.keymap.set("i", "<F5>", function()
+			local trigger_ai = function()
+				-- trigger_ai
+				require("codeium.virtual_text").cycle_or_complete()
+			end
+			trigger_ai()
+		end, { silent = true, expr = true })
 
 		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
@@ -49,13 +87,16 @@ return {
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
+				{ name = "codeium" }, -- Codeium AI
 			}),
 
 			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
 				format = lspkind.cmp_format({
+					mode = "symbol",
 					maxwidth = 50,
 					ellipsis_char = "...",
+					symbol_map = { Codeium = "☻ " },
 				}),
 			},
 		})
